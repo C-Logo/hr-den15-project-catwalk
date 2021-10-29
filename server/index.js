501bea6e2e798b6f3395fc4df2a7fc075b12e4c5
Ian <ianswensson@gmail.com>
2021-10-28 22:24:11 -0600

    Some progress

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const express = require('express'); // npm installed
-// const router = require('./routes.js');
+const router = require('./routes.js');
 
 const app = express();
-module.exports.app = app;
 app.set('port', 3000);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use('/reviews', router);
+app.use('/products', router);
+app.use('/qa/questions', router);
 
 // app.use('/*', router);
 
 app.listen(3000, () => {
   console.log('listening on port: ', 3000);
 });
+
+module.exports.app = app;
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const controller = require('./controllers');
 
-// const router = express.Router([options]);
+const router = express.Router();
 
 // router.get('/products', controller.products.get);
+
+router.route('/meta')
+  .get((req, res) => {
+    console.log(res);
+    res.send('test');
+  });
+
+module.exports = router;
