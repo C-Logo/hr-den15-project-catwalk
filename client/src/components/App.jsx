5b6f763721e82256dd8f3eef6913c87bc3b3867d
Ian <ianswensson@gmail.com>
2021-10-28 21:04:32 -0600

    React rendering booya

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Overview from './Overview.jsx';
+import Questions from './Questions.jsx';
+import Review from './Reviews.jsx';
 
 export default function App() {
   // delcare state variables
