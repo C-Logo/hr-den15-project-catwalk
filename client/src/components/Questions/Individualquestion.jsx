092c965e22280c3617ea5fc0d7c5ef10c55d85f5
Michael Light <mklight06@gmail.com>
2021-10-30 10:33:34 -0600

    Created seperate funcitons for Q&A helpful click functions

--- client/src/components/Questions/Individualquestion.jsx
+++ client/src/components/Questions/Individualquestion.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 export default function IndividualQuestion() {
-  let [helpfulYes, setHelpfulYes] = useState(0);
+  let [helpfulQuestionYes, setHelpfulQuestionYes] = useState(0);
+  let [helpfulAnswerYes, setHelpfulAnswerYes] = useState(0);
+  const [report, setReport] = useState(0);
 
-  function handleOnClick() {
-    setHelpfulYes(helpfulYes += 1);
+  function handleQuestionOnClick() {
+    setHelpfulQuestionYes(helpfulQuestionYes += 1);
+  }
+  function handleAnswerOnClick() {
+    setHelpfulAnswerYes(helpfulAnswerYes += 1);
   }
 
   return (
@@ -12,12 +17,22 @@ export default function IndividualQuestion() {
       <div>Q:Who what which where why whether how?</div>
       <div>A:One two threee four five six seven</div>
       <div>Helpful? </div>
-      <span onClick={handleOnClick}>
+      <span onClick={handleQuestionOnClick}>
         Yes
         (
-        {helpfulYes}
+        {helpfulQuestionYes}
         )
       </span>
+      <div>
+        <span>by User1234, Octotober 30th, 2021 |</span>
+        <span> Helpful? </span>
+        <span onClick={handleAnswerOnClick}>
+          Yes
+          (
+          {helpfulAnswerYes}
+          )
+        </span>
+      </div>
     </div>
   );
 }
