65061e1dc6429cb8ba736861d581181774819932
Ian <ianswensson@gmail.com>
2021-10-30 10:58:32 -0600

    App css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import Overview from './Overview.jsx';
 import Questions from './Questions/Questions.jsx';
-import Review from './reviews/Reviews.jsx';
+import Reviews from './reviews/Reviews.jsx';
 
 export default function App() {
   // delcare state variables
 
   return (
-    <div>
+    <div id="mainContainer">
       <Overview />
       <Questions />
-      <Review />
+      <Reviews />
     </div>
   );
 }
