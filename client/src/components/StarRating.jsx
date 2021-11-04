278f908f8540a94dac6775c66526625fa8bd97e6
Ian <ianswensson@gmail.com>
2021-11-04 17:51:16 -0600

    Star graph thing working!

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 export default function StarRating(props) {
+  // props should have a single property named rating that is a value between 1 and 5
+  const starStyle = {
+    width: `${props.rating / 5 * 100 * (99 / 100)}%`,
+  };
+
   return (
-    <div className="Stars">
-      {`${props.rating} Stars`}
+
+    <div className="starContainer">
+      <div className="starBackground" />
+      <div className="starFiller" style={starStyle} />
+      <div className="starBorder" id="starBorder1" />
+      <div className="starBorder" id="starBorder2" />
+      <div className="starBorder" id="starBorder3" />
+      <div className="starBorder" id="starBorder4" />
+      <div className="starBorder" id="starBorder5" />
+      <div className="starForegroundv3" />
     </div>
   );
 }
